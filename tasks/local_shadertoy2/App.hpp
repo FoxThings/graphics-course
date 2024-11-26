97598353b355d159c10db1541dcb8cf70c01a20f
FoxThings <nik.zve54321@gmail.com>
2024-11-26 22:33:10 +0300

    Copied local_shadertoy1

---
+++ tasks/local_shadertoy2/App.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <etna/Window.hpp>
+#include <etna/PerFrameCmdMgr.hpp>
+#include <etna/ComputePipeline.hpp>
+#include <etna/Image.hpp>
+
+#include "wsi/OsWindowingManager.hpp"
+
+#include <etna/Sampler.hpp>
+
+
+class App
+{
+public:
+  App();
+  ~App();
+
+  void run();
+
+private:
+  void drawFrame();
+
+private:
+  const char* computeProgramName = "shader_toy";
+
+  OsWindowingManager windowing;
+  std::unique_ptr<OsWindow> osWindow;
+
+  glm::uvec2 resolution;
+  bool useVsync;
+
+  std::unique_ptr<etna::Window> vkWindow;
+  std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
+
+  etna::ComputePipeline pipeline;
+  etna::Image image;
+  etna::Sampler sampler;
+};
